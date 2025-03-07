da867cd62ed0eb2d632852b6df012f2dc2206fe8
matsumo0922 <caios.system@gmail.com>
2025-03-07 20:46:58 +0900

    add dome puree-kotlin interfaces

---
+++ puree/src/commonMain/kotlin/com/cookpad/puree/PureeLog.kt
@@ -0,0 +1,6 @@
+package com.cookpad.puree
+
+/**
+ * Log classes that are registered to PureeLogger must implement this interface.
+ */
+interface PureeLog
---
+++ puree/src/commonMain/kotlin/com/cookpad/puree/output/PureeBufferedLog.kt
@@ -0,0 +1,24 @@
+package com.cookpad.puree.output
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.json.JsonObject
+
+/**
+ * Log object that is buffered to a [com.cookpad.puree.kotlin.store.PureeLogStore].
+ * Used by [com.cookpad.puree.kotlin.output.PureeBufferedOutput]
+ *
+ */
+data class PureeBufferedLog(
+    /**
+     * The unique identifier of this log.
+     */
+    val id: Long = 0,
+    /**
+     * The date and time when this log is posted.
+     */
+    val createdAt: Instant,
+    /**
+     * The log serialized in JSON format.
+     */
+    val log: JsonObject,
+)
