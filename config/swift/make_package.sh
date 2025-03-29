0b322dd24851725b36c7a4cf687f38006a7e5ed6
matsumo0922 <caios.system@gmail.com>
2025-03-30 03:01:40 +0900

    add deployment workflows and update package script for new file paths

--- config/swift/make_package.sh
+++ config/swift/make_package.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+mv ../../puree/build/XCFrameworks/release/puree.xcframework.zip ./puree.xcframework.zip
+
 VERSION="$1"
-CHECKSUM=$(swift package compute-checksum ../../puree/build/XCFrameworks/release/puree.xcframework.zip)
+CHECKSUM=$(swift package compute-checksum ./puree.xcframework.zip)
 
 npx ejs Package.swift.ejs -i '{"version": "'$VERSION'", "checksum": "'$CHECKSUM'"}' -o ../../Package.swift
