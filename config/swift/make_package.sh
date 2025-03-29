102b4933a888078ef6b224dd193a0854d7cbd8e2
matsumo0922 <caios.system@gmail.com>
2025-03-29 22:17:21 +0900

    fixed nits

--- config/swift/make_package.sh
+++ config/swift/make_package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=$(cat VERSION)
+VERSION="$1"
 CHECKSUM=$(swift package compute-checksum ../../puree/build/XCFrameworks/release/puree.xcframework.zip)
 
 npx ejs Package.swift.ejs -i '{"version": "'$VERSION'", "checksum": "'$CHECKSUM'"}' -o ../../Package.swift
