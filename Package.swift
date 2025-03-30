72323b54a05fcc1a24767d44d811043ad419bd9c
matsumo0922 <56629437+matsumo0922@users.noreply.github.com>
2025-03-30 08:11:18 +0000

    [create-pull-request] automated change

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "puree-kmp",
-            url: "https://github.com/cookpad/puree-kmp/releases/download/0.0.8/puree.xcframework.zip",
-            checksum: "411f239368158f931a37729bc07c0d2e4d8eef64caa79628d3d7bab7f4848603"
+            url: "https://github.com/cookpad/puree-kmp/releases/download/0.0.9/puree.xcframework.zip",
+            checksum: "a891024688d98f5ecadea7045a85eb169e8ea2a8c7a1d4b465e992750e2c561b"
         ),
     ]
 )
