f8ecbbadf4c28e1966aceeace3936be7cd5152bd
daichi-matsumoto <daichi-matsumoto@cookpad.com>
2025-08-06 11:46:15 +0900

    feat: upgrade Java and Kotlin compiler options to version 21

--- build-logic/src/main/kotlin/primitive/android/AndroidCommonPlugin.kt
+++ build-logic/src/main/kotlin/primitive/android/AndroidCommonPlugin.kt
@@ -3,8 +3,6 @@ package primitive.android
 import com.cookpad.puree.kmp.isMultiplatformProject
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import primitive.kmp.kotlin
 
 class AndroidCommonPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -18,19 +16,6 @@ class AndroidCommonPlugin : Plugin<Project> {
                 apply("kotlinx-serialization")
                 apply("project-report")
                 apply("com.google.devtools.ksp")
-            }
-
-            kotlin {
-                androidTarget {
-                    compilerOptions {
-                        jvmTarget.set(JvmTarget.JVM_17)
-                        freeCompilerArgs.addAll(
-                            "-Xopt-in=kotlinx.coroutines.FlowPreview",                          // required for flow.debounce
-                            "-Xopt-in=kotlinx.coroutines.DelicateCoroutinesApi",                // Global scope
-                            "-Xopt-in=androidx.compose.foundation.ExperimentalFoundationApi"    // Compose
-                        )
-                    }
-                }
             }
         }
     }
